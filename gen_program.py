1876bc10397bfcc2ecbad8d3629ec1ca6f79175b
uwaces <uwaces@gmail.com>
2018-12-19 19:47:34 -0500

    Added Gen program

---
+++ gen_program.py
@@ -0,0 +1,9 @@
+with open("program.txt") as file:
+        byte = 0
+        format_dmem = '      dmemBytes({})  <= "{}";'
+        for line in file:
+                print(format_dmem.format(byte, line[0:8]))
+                print(format_dmem.format(byte+1, line[8:16]))
+                print(format_dmem.format(byte+2, line[16:24]))
+                print(format_dmem.format(byte+3, line[24:32]))
+                byte+= 4
